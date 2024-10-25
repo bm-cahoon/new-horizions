c266dba9bbeccff4492f9a44742d9379b44ba3a3
Ben Cahoon <ben.cahoon@appdat.jsc.nasa.gov>
2024-10-25 10:02:04 -0500

    removing ai

--- LLAMA/LLAMA_install.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_name = "meta-llama/Llama-2-7b"  # Example, check the exact model
-model = AutoModelForCausalLM.from_pretrained(3.2)
-tokenizer = AutoTokenizer.from_pretrained(3.2)
